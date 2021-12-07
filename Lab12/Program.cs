f0bc96df43f2becaa476b1a6862e08be7884f0e3
Евгения Волкова <genya.volkova@gmail.com>
2021-12-07 19:26:03 +0300

    Correction1

--- Lab12/Program.cs
+++ Lab12/Program.cs
@@ -10,40 +10,48 @@ namespace Lab12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Длина окружности= {0}", Circle.L);
-            Console.WriteLine("Площадь круга= {0}", Circle.S);
-            Console.WriteLine(Circle.X);
+            Console.WriteLine("Введите значение радиуса");
+            Circle.r= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Длина окружности= ");
+            Console.WriteLine(Circle.length());
+            Console.Write("Площадь окружности= ");
+            Console.WriteLine(Circle.square());
+            Console.WriteLine("Введите координаты точки Х");
+            Circle.x= Convert.ToInt32(Console.ReadLine());
+            Circle.y= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Circle.point());
             Console.ReadKey();
         }
         public static class Circle
         {
             public static double L;
             public static double S;
-            public static double l;
-            public static double s;
             public static double R;
             public static string X;
             public static double x;
             public static double y;
             public static int r;
-            static double length()
+            public static double length()
             {
-                l = 2 * r * Math.PI;
-                L = l;
+                L = 2 * r * Math.PI;
+               
                 return L;
             }
-            static double square()
+            public static double square()
             {
                 S = Math.PI * Math.Pow(r, 2);
-                S = s;
                 return S;
             }
-            static string point()
+            public static string point()
             {
                 R = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
                 if (r == R)
                 {
                     X = "Точка принадлежит кругу";
+                }
+                else 
+                {
+                    X = "Точка не принадлежит кругу";
                 }
                 return X;
             }
